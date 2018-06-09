c3aab81a371ac231fc47e277faa3b537315d6384
kljajic <kljajic77@gmail.com>
2018-06-09 14:35:36 +0200

    Added MediaPreviewActivity and corresponding Fragment. Need to fix bug with displaying profile picture and username on MediaPreviewFragment.

---
+++ app/src/main/java/com/studder/fragments/MediaPreviewFragment.java
@@ -0,0 +1,134 @@
+package com.studder.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.studder.R;
+import com.studder.model.Media;
+import com.studder.utils.ClientUtils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MediaPreviewFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MediaPreviewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MediaPreviewFragment extends Fragment {
+
+    private ImageView userProfilePicture;
+    private TextView usernameTextView;
+    private ImageView mediaImageView;
+    private TextView mediaDescriptionTextView;
+    private TextView mediaDateAddedTextView;
+
+    private OnFragmentInteractionListener mListener;
+
+    public MediaPreviewFragment() {
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment MediaPreviewFragment.
+     */
+    public static MediaPreviewFragment newInstance() {
+        MediaPreviewFragment fragment = new MediaPreviewFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_media_preview, container, false);
+        userProfilePicture = view.findViewById(R.id.media_preview_fragment_profile_picture);
+        usernameTextView = view.findViewById(R.id.media_preview_fragment_username_text_view);
+        mediaImageView = view.findViewById(R.id.media_preview_fragment_media_image_view);
+        mediaDescriptionTextView = view.findViewById(R.id.media_preview_fragment_description_text_view);
+        mediaDateAddedTextView = view.findViewById(R.id.media_preview_fragment_media_date_added_text_view);
+
+        Intent intent = getActivity().getIntent();
+        String username = intent.getStringExtra("userUsername");
+
+        String encodedProfileImage = ClientUtils.readMediaFromFile(intent.getStringExtra("userProfileImagePath"));
+        Bitmap profilePicture = ClientUtils.getBitmapForMedia(encodedProfileImage);
+        Long mediaId = intent.getLongExtra("mediaId", 0L);
+
+        String encodedMedia = ClientUtils.readMediaFromFile(intent.getStringExtra("mediaPath"));
+        Bitmap mediaBitmap = ClientUtils.getBitmapForMedia(encodedMedia);
+        Long mediaDateAdded = intent.getLongExtra("mediaDateAdded", 0L);
+        String mediaDescription = intent.getStringExtra("mediaDescription");
+
+        userProfilePicture.setImageBitmap(profilePicture);
+        usernameTextView.setText(username);
+        mediaImageView.setImageBitmap(mediaBitmap);
+        mediaDescriptionTextView.setText(mediaDescription);
+        mediaDateAddedTextView.setText(ClientUtils.formatDateTime(mediaDateAdded));
+
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
--- app/src/main/java/com/studder/utils/ClientUtils.java
+++ app/src/main/java/com/studder/utils/ClientUtils.java
@@ -1,6 +1,17 @@
 package com.studder.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.util.Base64;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -10,4 +21,85 @@ public class ClientUtils {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.GERMAN);
         return sdf.format(new Date(time));
     }
+
+    public static String saveMediaToPhoneStorage(String directory, String filename, String mediaEncoded){
+        String sdPath = Environment.getExternalStorageDirectory().getPath();
+
+        File dir = new File(sdPath + "/Studder/" + directory);
+        File file = new File(dir, filename + ".jpg");
+
+        FileOutputStream fileOutputStream = null;
+        try {
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(mediaEncoded.getBytes());
+            fileOutputStream.flush();
+        } catch (Exception e) {
+            return null;
+        }finally {
+            if(fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        return file.getAbsolutePath();
+    }
+
+    public static String readMediaFromFile(String path) {
+        File file = new File(path);
+        if(!file.exists()) {
+            return null;
+        }
+
+        StringBuilder fileContent = new StringBuilder();
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while((line = bufferedReader.readLine())!= null) {
+                fileContent.append(line);
+            }
+        } catch (Exception e) {
+            return null;
+        }finally {
+            if(bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    return null;
+                }
+            }
+        }
+
+        return fileContent.toString();
+    }
+
+    public static Bitmap getBitmapForMedia(String encodedMedia) {
+        byte[] bitmapBytes = Base64.decode(encodedMedia, Base64.DEFAULT);
+        Bitmap bmp = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+        bmp = bmp.createScaledBitmap(bmp, 200, 200, false);
+        return BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+    }
+
+    public static int calculateAge(Date date) {
+        Calendar calendar1 = Calendar.getInstance();
+        Calendar calendar2 = Calendar.getInstance();
+        calendar1.setTime(date);
+        int diff = calendar2.get(Calendar.YEAR) - calendar1.get(Calendar.YEAR);
+        if (calendar1.get(Calendar.MONTH) > calendar2.get(Calendar.MONTH) || (calendar1.get(Calendar.MONTH)
+                == calendar2.get(Calendar.MONTH) && calendar1.get(Calendar.DATE) > calendar2.get(Calendar.DATE))) {
+            diff--;
+        }
+        return diff;
+    }
+
 }
